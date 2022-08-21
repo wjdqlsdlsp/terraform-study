9e3c99dfc13cb570c7909840c5b5c48cc89af97a
jeongbin <wjdqlsdlsp@naver.com>
2022-08-22 01:22:52 +0900

    220822

--- for/count.tf
+++ for/count.tf
@@ -1,18 +1,8 @@
-variable "user_names" {
-  description = "Create IAM users with these names"
-  type = list(string)
-  default = ["neo", "trinity", "morphus"]
-}
-
-resource "aws_iam_user" "example" {
-  count = length(var.user_names)
-  name = var.user_names[count.index]
-}
-
-#output "neo_arn" {
-#  value = aws_iam_user.example[0].arn
+#resource "aws_iam_user" "example" {
+#  count = length(var.user_names)
+#  name = var.user_names[count.index]
 #}
 
-output "all_arns" {
-  value = aws_iam_user.example[*].arn
-}
+#output "all_arns" {
+#  value = aws_iam_user.example[*].arn
+#}
---
+++ for/for_each.tf
@@ -0,0 +1,12 @@
+resource "aws_iam_user" "example" {
+  for_each = toset(var.user_names)
+  name = each.value
+}
+
+output "all_users" {
+  value = aws_iam_user.example
+}
+
+output "all_arns" {
+  value = values(aws_iam_user.example)[*].arn
+}
